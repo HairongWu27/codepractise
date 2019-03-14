7a022fb365898a53b41ada559e3fb39609f21d8f
helen <whr1999@gmail.com>
2019-03-14 09:02:15 -0700

    modify twosun

--- twosum.js
+++ twosum.js
@@ -3,7 +3,7 @@ var nums=[2, 7, 11, 15];
 function twosun (arry, target){
  
   for( var i=0, j=arry.length;i<j;){
-    var tmp= target-arry[i];
+    var tmp= target-arry[i];//avoid overflow
     console.log("i "+i+" j "+j+" tmp "+tmp);
     if(tmp === arry[j]) return true;
     else if(tmp > arry[j]) ++i;
